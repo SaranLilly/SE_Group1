460799c1e380bf52b519e69dd2c363f06e276fa6
SaranLilly <117520812+SaranLilly@users.noreply.github.com>
2024-03-10 21:36:16 +0700

    setPages

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('contact');
-});
+// Route::get('/', function () {
+//     return view('contact');
+// });
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +17,6 @@ Route::get('/', function () {
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
