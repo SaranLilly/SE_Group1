1e269992146b9edbbf0fde5ccd02b7479c9adce7
NuiKn <kittipong.nui01@gmail.com>
2024-03-19 17:02:11 +0700

    แก้ไข title
    fefeffsdffsdsdogsdogdsg

--- resources/views/Setkpi/index.blade.php
+++ resources/views/Setkpi/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <title>Document</title>
+    <title>AAAAAAA</title>
 </head>
 <body>
     <h1>Setkpi</h1>
