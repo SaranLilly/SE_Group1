51bf20384abafea512a603baa9c0ca1d975e5429
SaranLilly <117520812+SaranLilly@users.noreply.github.com>
2024-03-20 20:45:20 +0700

    EMP suss

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </head>
 <body>
+    <div class="container">
     <h1>Edit Employee</h1>
     <div>
         @if($errors->any())
@@ -18,7 +22,7 @@
         </ul>
         @endif
     </div>
-    <form method="POST" action="{{ route('employee.update',['employee'=>$employee->empID]) }}">
+    <form class="row g-3" method="POST" action="{{ route('employee.update',['employee'=>$employee->empID]) }}">
 
         @csrf
         @method('put')
@@ -26,7 +30,7 @@
             <label>Prefix</label>
             <input type="text" name="prefix" placeholder="prefix" value="{{ $employee->prefix }}">
         </div> --}}
-        <div>
+        <div class="form-label" class="col-md-6">
             <label for="prefix">Prefix</label>
             <select name="prefix" id="prefix">
                 <option value="นาย">นาย</option>
@@ -34,43 +38,43 @@
                 <option value="นางสาว">นางสาว</option>
             </select>
         </div>
-        <div>
+        <div class="form-label">
             <label>Firstname</label>
             <input type="text" name="firstName" placeholder="firstName" value="{{ $employee->firstName }}">
         </div>
-        <div>
+        <div class="form-label">
             <label>Lastname</label>
             <input type="text" name="lastName" placeholder="lastName" value="{{ $employee->lastName }}">
         </div>
-        <div>
+        <div class="form-label">
             <label>Nickname</label>
             <input type="text" name="nickname" placeholder="nickname" value="{{ $employee->nickname }}">
         </div>
-        <div>
+        <div class="form-label">
             <label>userName</label>
             <input type="text" name="userName" placeholder="userName" value="{{ $employee->userName }}">
         </div>
-        <div>
+        <div class="form-label">
             <label>Password</label>
             <input type="password" name="password" placeholder="password" value="{{ $employee->password }}"> 
         </div>
-        <div>
+        <div class="form-label">
             <label>PhoneNumber</label>
             <input type="text" name="phoneNumber" placeholder="phoneNumber" value="{{ $employee->phoneNumber }}">
         </div>
-        <div>
+        <div class="form-label">
             <label>IdCard</label>
             <input type="text" name="idCard" placeholder="idCard" value="{{ $employee->idCard }}">
         </div>
-        <div>
+        <div class="form-label">
             <label>Birth</label>
             <input type="date" name="birth" placeholder="birth" value="{{ $employee->birth }}">
         </div>
-        <div>
+        <div class="form-label">
             <label>Email</label>
             <input type="text" name="email" placeholder="email" value="{{ $employee->email }}">
         </div>
-        <div>
+        <div class="form-label">
             <label>Position </label>
             <select name="positionID">
                 @foreach($positions as $position)
@@ -78,7 +82,7 @@
                 @endforeach
             </select>
         </div>
-        <div>
+        <div class="form-label">
             <label>SaleTeam </label>
             <select name="saleTeamID">
                 @foreach($saleteams as $saleteam)
@@ -94,9 +98,10 @@
             <label>SaleTeamID </label>
             <input type="text" name="saleTeamID" placeholder="saleTeamID" value="{{ $employee->saleTeamID }}">
         </div> --}}
-        <div>
+        <div class="col-12">
             <input type="submit" value="Update">
         </div>
     </form>
+</div>
 </body>
 </html>
--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -5,12 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </head>
 <body>
     <h1>Employee</h1>
 
     <div>
-        <a href="{{ route('employee.create') }}">New Employee</a>
+        <a class="btn btn-primary" href="{{ route('employee.create') }}">New Employee</a>
     </div>
     
     <div>
@@ -21,7 +24,7 @@
         @endif
     </div>
     <div>
-        <table border="1">
+        <table border="1" class="table table-striped">
             <tr>
                 {{-- <th>ลำดับ</th> --}}
                 <th>คำนำหน้าชื่อ</th>
@@ -59,13 +62,13 @@
                     
                     
                     <td>
-                        <a href="{{ route('employee.edit',['employee' => $employee->empID]) }}">Edit</a>
+                        <a class="btn btn-success" href="{{ route('employee.edit',['employee' => $employee->empID]) }}">Edit</a>
                     </td>
                     <td>
                         <form method="POST" action="{{ route('employee.destroy',['employee'=> $employee->empID]) }}">
                             @csrf
                             @method('delete')
-                            <input type="submit" value="Delete">
+                            <input type="submit" value="Delete" class="btn btn-danger">
                         </form>
                     </td>
                 </tr>
