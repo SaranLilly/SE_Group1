9459e333daa7def46a56e21660f2c9065f058c0e
Meowwwza <sorawit.3p@gmail.com>
2024-03-25 20:09:35 +0700

    เพิ่ม layout หน้า create.blade.php

--- resources/views/evaluation/create.blade.php
+++ resources/views/evaluation/create.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
+@section('title','การประเมิน')
+@section('content')
     <h1>create</h1>
     <form method="post" action="{{route('evaluation.store')}}">
         @csrf
@@ -193,5 +187,4 @@
           </table>
         </div>
     </form>
-</body>
-</html>
+@endsection
